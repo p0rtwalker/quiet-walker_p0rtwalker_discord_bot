9ee4f7843d85c7d2b387462b06692cae207d205c
p0rtw4lk3r <150483877+p0rtwalker@users.noreply.github.com>
2024-07-16 05:23:29 +0300

    Add files via upload

---
+++ discord.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def hello_bot(ctx):
+    await ctx.send('Hello Mr. Haxor')
+
+bot.run('') # I'm not going to share my token.  
+
